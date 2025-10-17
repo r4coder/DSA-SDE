6de30d0900d0b76903046ce7c50a7b95cacac6c8
r4coder <ritheeshk2003@gmail.com>
2025-10-18 00:10:07 +0530

    Add maximum subarray sum calculation
    Implement Kadane's algorithm to find the maximum subarray sum.

--- 10-day/max subarray sum.cpp
+++ 10-day/max subarray sum.cpp
@@ -1,1 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+int main() {
+      int n;
+      cin>>n;
+      int sum=0;
+      int maxi=INT_MIN;
+      vector<int>v(n);
+      
+      for(int i=0;i<n;i++)
+      {
+          cin>>v[i];
+      }
+      
+      unordered_map<int,int>m;
+      
+      for(int i=0;i<n;i++)
+      {
+             sum+=v[i];
+             maxi=max(maxi,sum);
+             
+             if(sum<0)
+             {
+                 sum=0;
+             }
+             
+        
+         
+      }
+      
+      cout<<maxi;
+      
+      
+      
+          
+
+
+    return 0;
+}

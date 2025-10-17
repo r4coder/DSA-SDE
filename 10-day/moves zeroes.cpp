2dc13c58f68deb46a5a4af2905639adf3fbdef9c
r4coder <ritheeshk2003@gmail.com>
2025-10-18 00:25:55 +0530

    Implement zeroes movement in array

--- 10-day/moves zeroes.cpp
+++ 10-day/moves zeroes.cpp
@@ -1,1 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+int main() {
+      int n;
+      cin>>n;
+      vector<int>v(n);
+      vector<int>temp;
+      int count=0;
+      
+      for(int i=0;i<n;i++)
+      {
+          cin>>v[i];
+      }
+      
+     
+      
+      for(int i=0;i<n;i++)
+      {
+             if(v[i]>0)
+             {
+                 temp.push_back(v[i]);
+                 count++;
+             }
+         
+      }
+      
+      for(int i=0;i<n-count;i++)
+      {
+           temp.push_back(0);
+      }
+      
+      for(int i=0;i<n;i++)
+      {
+          cout<<temp[i];
+      }
+      
+      
+      
+          
+
+
+    return 0;
+}

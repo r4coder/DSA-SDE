ba25d233ddd003f8c1c2676257d39925a42d5a8b
r4coder <ritheeshk2003@gmail.com>
2025-10-18 00:58:13 +0530

    Add two sum solution in two_sum.cpp
    Implement two sum algorithm using unordered_map.

--- amazon/two sum.cpp
+++ amazon/two sum.cpp
@@ -1,1 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+int main() {
+      int n;
+      cin>>n;
+      int T;
+      cin>>T;
+      vector<int>v(n);
+      
+      for(int i=0;i<n;i++)
+      {
+          cin>>v[i];
+      }
+      
+      unordered_map<int,int>m;
+      
+      for(int i=0;i<n;i++)
+      {
+          int c=T-v[i];
+          
+          if(m.find(c)!=m.end())
+          {
+              cout<<i<<"\n";
+              cout<<m[c];
+              break;
+          }
+          m[v[i]]=i;
+      }
+      
+      
+      
+          
+
+
+    return 0;
+}

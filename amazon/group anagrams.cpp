918a0a1308f2ed921e0fc81b541201772fcfb7f8
r4coder <ritheeshk2003@gmail.com>
2025-10-18 19:55:48 +0530

    Implement anagram grouping from user input
    This program groups anagrams from a list of strings input by the user. It uses a hash map to store sorted strings as keys and their corresponding anagrams as values.

--- amazon/group anagrams.cpp
+++ amazon/group anagrams.cpp
@@ -1,1 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+int main() {
+      int n;
+      cin>>n;
+      vector<string>strs(n);
+      vector<vector<string>>ans;
+      
+      unordered_map<string,vector<string>>mp;
+      
+      for(int i=0;i<n;i++)
+      {
+          cin>>strs[i];
+          
+      }
+      
+      for(int i=0;i<n;i++)
+      {
+          string s=strs[i];
+          sort(begin(s),end(s));
+          mp[s].push_back(strs[i]);
+          
+      }
+      
+      for(auto it:mp)
+      {
+          ans.push_back(it.second);
+      }
+      
+      
+    for (auto &group : ans) {
+        for (auto &word : group) {
+            cout << word << " ";
+        }
+        cout << "\n";
+    }
+      
+          
+
+
+    return 0;
+}

1d6a813d44842fb3ce1cd58c9c11f6b5ba3e448a
r4coder <ritheeshk2003@gmail.com>
2025-10-22 13:45:16 +0530

    Implement longest substring without repeating characters

--- amazon/Longest Substring Without Repeating Characters.cpp
+++ amazon/Longest Substring Without Repeating Characters.cpp
@@ -1,1 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  string s;
+  int i=0,j=0;
+  cin>>s;
+  int n=s.size();
+  int maxi=INT_MIN;
+  int leng=0;
+  unordered_map<char,int>mp;
+  
+  while(j<n)
+  {
+       mp[s[j]]++;
+       
+       if(mp[s[j]]>1)
+       {
+           mp[s[i]]--;
+           i++;
+          
+       }
+       
+       
+           j++;
+           maxi=max(maxi,j-i+1);
+      
+       
+       
+        
+  }
+  
+  cout<<maxi;
+  
+  
 
+    return 0;
+}
